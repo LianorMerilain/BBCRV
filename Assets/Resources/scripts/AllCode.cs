201e0deda70db7a0232e528c6667a23fbeb751de
MoonlitedAvira <dimazhukovsky505@gmail.com>
2024-07-01 16:56:06 +0300

    new
    y

--- Assets/Resources/scripts/AllCode.cs
+++ Assets/Resources/scripts/AllCode.cs
@@ -1,26 +1,23 @@
 using System.Collections; //всЄ сделано в одном скрипте, так как € не гимнаст, мне не нужен гибкий код, мне нужен рабочий код.                 
-using System.Collections.Generic; //“ак как € почти англичанин, в моих коментари€х можно заметить немного английского вайба.
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 public class AllCode : MonoBehaviour
 {
-    //Volume
-    public AudioClip Volume;
-    public AudioSource Audio; //чЄт западает, в godbox нужно Audio каждый раз выбирать, при загрузке сцены, это своего рода фича
-    //Not UI
-    private readonly List<Vector3> _chestPositionPoint = new();
+    public AudioClip Volume; //Volume
+    private AudioSource _audio;
+
+    public Vector3[] PositionsArray;
+    private readonly Queue _chestPositionPoint = new();
     private LineRenderer _lineRenderer;
     private float _speed;
     private bool _isMoving = false;
     public GameObject Player;
-    //UI
-
-    public Text ScoreText;
+    public Text ScoreText; //UI
     public Scrollbar SdeedScrollbar;
-    private void Start() //махинации with lines po большей части
+    private void Start() 
     {
-        Audio = GetComponent<AudioSource>();
         _lineRenderer = GetComponent<LineRenderer>();
         _lineRenderer.endWidth = 0.5f;
         _lineRenderer.startWidth = 0.5f;
@@ -28,19 +25,21 @@ public class AllCode : MonoBehaviour
     }
     void Update()
     {
-
+        Queue(_chestPositionPoint);
         UI();
-        Line(_chestPositionPoint);
-        if (_isMoving)//Ћогика dvijenie персонажа 
+        //Queue(_chestPositionPoint);
+        /*if (_isMoving)
         {
-            if ((Vector2)Player.transform.position == (Vector2)_chestPositionPoint[0])
+            if ((Vector2)Player.transform.position == (Vector2)PositionsArray[0])
             {
-                _chestPositionPoint.RemoveAt(0);
+            
                 _isMoving = false;
+                Debug.Log("0");
             }
             else
             {
-                Player.transform.position = Vector2.MoveTowards(Player.transform.position, _chestPositionPoint[0], _speed * Time.deltaTime);
+                Player.transform.position = Vector2.MoveTowards(Player.transform.position, PositionsArray[0], _speed * Time.deltaTime);
+                Debug.Log("1");
             }
         }
         else
@@ -48,29 +47,54 @@ public class AllCode : MonoBehaviour
             if (_chestPositionPoint.Count > 0)
             { 
                 _isMoving = true;
+                Debug.Log("2");
+            }
+        }*/
+        if (_isMoving)
+        {
+            PositionsArray = new Vector3[_chestPositionPoint.Count];
+            bool ArePositionsClose(Vector2 pos1, Vector2 pos2, float tolerance = 0.01f)
+            {
+                return Vector2.Distance(pos1, pos2) < tolerance;
+            }
+            if (ArePositionsClose((Vector2)Player.transform.position, (Vector2)PositionsArray[0]))
+            {
+                Debug.Log("0");
+                _chestPositionPoint.Dequeue();
+                _isMoving = false;
+            }
+            else
+            {
+                Player.transform.position = Vector2.MoveTowards((Vector2)Player.transform.position, PositionsArray[0], _speed * Time.deltaTime);
+                Debug.Log("1");
             }
         }
-            if (Input.GetMouseButtonDown(1))//Creature point po click mouse(not life)
-            {
+        else
+        {
+            if (_chestPositionPoint.Count > 0) _isMoving = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
             PointCreator();
-            }
+        }
     }
-    public void PointCreator()//one смысловой блок
+    public void PointCreator()
     {
-     Vector3 PositionPoint = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-     _chestPositionPoint.Add(PositionPoint);   //Zanosim value в list           
-        Audio.PlayOneShot(Volume);
+        Vector3 PositionPoint = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _chestPositionPoint.Enqueue(PositionPoint);
+        //_audio.PlayOneShot(Volume);
     }
-    private void Line(List<Vector3> list)//two смысловой блок (Ќастраиваем logic ways к point)
+    public void Queue(Queue queue)
     {
-        _lineRenderer.positionCount = list.Count+1; 
-        list.Insert(0, (Vector2)Player.transform.position);
-        _lineRenderer.SetPositions(list.ToArray());
-        list.RemoveAt(0);
+        _lineRenderer.positionCount = queue.Count + 1;
+        queue.Enqueue(Player.transform.position); //list.Insert(0, (Vector2)Player.transform.position); ERROR
+        _lineRenderer.SetPositions(PositionsArray); // _lineRenderer.SetPositions(list.ToArray());
+        queue.Dequeue();
+
     }
-    public void UI()//three смысловой блок (module UI(старалс€))
+    public void UI()
     {
         ScoreText.text = "active click: " + _chestPositionPoint.Count;
-        _speed = SdeedScrollbar.value*10;
+        _speed = SdeedScrollbar.value * 10;
     }
 }
